993bd5f105164a340d919eae2b66c97906139feb
dgross881 <dgross881@gmail.com>
2014-07-28 09:51:30 +0800

    add comment box

--- spec/features/articles/articles_spec.rb
+++ spec/features/articles/articles_spec.rb
@@ -1,2 +1,13 @@
 require 'spec_helper' 
 
+require 'spec_helper'
+
+describe "Article" do
+  it 'should validate presence of title' do
+      let!(:article) { Article.create
+      post.valid?
+      expect(post.errors.messages[:title]).to include "can't be blank"
+    end
+  end
+
+
